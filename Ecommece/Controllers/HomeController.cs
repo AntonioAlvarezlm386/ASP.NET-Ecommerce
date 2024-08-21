45ada687d7f5cdb9aef7897dbd11286d60c5f199
AntonioAlvarezlm386 <antoniolm386@gmail.com>
2024-08-21 07:59:09 -0600

    chore(namespaces rename): the namespaces of the service, data and entity layer were renamed

--- Ecommece/Controllers/HomeController.cs
+++ Ecommece/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using EntityLayout;
+using EntityLayer;
 using BussinesLayer;
 
 namespace Ecommece.Controllers
--- EntityLayer/CLIENT.cs
+++ EntityLayer/CLIENT.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayout
+namespace EntityLayer
 {
     public class CLIENT
     {
